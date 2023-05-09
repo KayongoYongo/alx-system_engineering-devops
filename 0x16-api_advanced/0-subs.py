ace4a7da7640e07af630a17f45a08b4aedae2411
KayongoYongo <sknsammie10@gmail.com>
2023-05-09 17:41:59 +0000

    subscriber_count

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Returns the number of subscribers"""
+
+"""Returns the number of subscribers"""
 
 import requests
 
@@ -7,10 +8,9 @@ import requests
 def number_of_subscribers(subreddit):
     """The function checks a subreddit and returns subscribed users"""
 
-    response = requests.get(
-        f"https://www.reddit.com/r/{subreddit}/about.json",
-        headers={'User-Agent': 'MyAPI/0.0.1'},
-        allow_redirects=False)
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     data = response.json().get("data")
 
