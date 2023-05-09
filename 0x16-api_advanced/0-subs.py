b144b15569613f8100cd40a8d0f616301d12fc28
KayongoYongo <sknsammie10@gmail.com>
2023-05-09 17:34:14 +0000

    subscriber_count

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,16 +6,20 @@ import requests
 
 def number_of_subscribers(subreddit):
     """The function checks a subreddit and returns subscribed users"""
-    try:
-        response = requests.get(
-            f"https://www.reddit.com/r/{subreddit}/about.json",
-            headers={'User-Agent': 'MyAPI/0.0.1'})
 
-        subreddit_data = response.json()['data']
+    response = requests.get(
+        f"https://www.reddit.com/r/{subreddit}/about.json",
+        headers={'User-Agent': 'MyAPI/0.0.1'},
+        allow_redirects=False)
 
-        subreddit_subscriber_count = subreddit_data['subscribers']
+    data = response.json().get("data")
 
-        return (subreddit_subscriber_count)
+    if data is None:
+        return (0)
 
-    except KeyError:
+    count = data.get("subscribers")
+
+    if response.status_code == 200:
+        return (count)
+    else:
         return (0)
